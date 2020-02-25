14e0b0f9d356645532fa1e5c2d55154de2e021f5
ultraconformist <ultraconformist@gmx.com>
2020-02-24 22:39:45 -0500

    Completed PS1B; this commit will have all my notes in it I took while trying to solve it. Will commit again cleaned up.

--- ps6/ps1b.py
+++ ps6/ps1b.py
@@ -20,7 +20,7 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    global eggs_taken   # Testing variable to test greedy algorithm
+    #global eggs_taken   # Testing variable to test greedy algorithm
     
     # Greedy algorithm recursively done:
     # Base case 1: If target_weight == 0, return the number of eggs 
@@ -32,17 +32,65 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     # with the new list
     # Need to do this, and then memoize the recursive solution?
 
-    if target_weight == 0:
-        return eggs_taken
-    elif target_weight - egg_weights[-1] >= 0:
-        updated_target_weight = target_weight - egg_weights[-1]
-        eggs_taken += 1
-        return dp_make_weight(egg_weights, updated_target_weight)
-    else:
-        return dp_make_weight(egg_weights[:-1], target_weight)
+    #if target_weight == 0:
+    #    return eggs_taken
+    #elif target_weight - egg_weights[-1] >= 0:
+    #    updated_target_weight = target_weight - egg_weights[-1]
+    #    eggs_taken += 1
+    #    return dp_make_weight(egg_weights, updated_target_weight)
+    #else:
+    #    return dp_make_weight(egg_weights[:-1], target_weight)
     
     # Okay, this works as expected; how can this be made dynamic?
-    pass
+    # Let's try to rewrite this as a brute force algorithm, recursively.
+    # Base case 1: if target_weight == 0, return the number of eggs taken.
+    # Recursive solution: For each weight in egg_weights, subtract that weight
+    # from target_weight and = to updated_target_weight, add one to eggs taken,
+    # and repeat with new target weight?
+    #if target_weight == 0:
+    #    return eggs_taken
+    #for weight in egg_weights:
+    #    updated_target_weight = target_weight-weight
+    #    print(updated_target_weight)
+    #    eggs_taken += 1
+    #    return dp_make_weight(egg_weights, updated_target_weight)
+    
+    # This needs to be based on a decision tree that evaluates recursively...
+    #eggs_taken = 0
+    #if target_weight == 0:
+    #    return eggs_taken
+    #for weight in egg_weights:
+    #    if weight <= target_weight:
+    #       updated_target_weight = target_weight - weight
+    #        return dp_make_weight(egg_weights, updated_target_weight)
+    #        print(updated_target_weight)
+    #    else:
+    #        return dp_make_weight(egg_weights, target_weight)
+    # Dynamic programming involves the same thing 
+    
+    choice_list = []
+    
+    # First, check to see if this problem is already solved.
+    try:
+        return memo[target_weight]
+    # If memo[target_weight] doesn't exist, then solve for this weight
+    except KeyError:
+        # Base case
+        if target_weight == 0:
+            return 0
+        # Choose a weight in the list of egg weights, subturact it from target,
+        # and if the updated weight isn't below 0, then we're not done yet;
+        # recursively call egg weight.
+        # Finally, return the recursive call that has the lowest weight, + 1
+        # because this call was one additional egg, obviously.
+        # Also add this solution to the memo.
+        else:
+            for weight in egg_weights:
+                if target_weight - weight >= 0:
+                    updated_weight = target_weight - weight
+                    choice_list.append(dp_make_weight(egg_weights, updated_weight, memo))
+            memo[target_weight] = (min(choice_list) + 1)
+            return (min(choice_list) + 1)
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
